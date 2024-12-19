ee1667312f499ced8ded8d0d1cd174e9a43e89e3
Verkyav Peter <verkyavpeter@gmail.com>
2024-12-19 20:23:01 +0100

    images configuration

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,55 @@ const nextConfig: NextConfig = {
         port: '3000',
         pathname: '/images/**',
       },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/books/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/profile/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/book-images/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/profile-images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'sea-lion-app-jl247.ondigitalocean.app',
+        pathname: '/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'sea-lion-app-jl247.ondigitalocean.app',
+        pathname: '/profile/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'sea-lion-app-jl247.ondigitalocean.app',
+        pathname: '/book-images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'sea-lion-app-jl247.ondigitalocean.app',
+        pathname: '/profile-images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'sea-lion-app-jl247.ondigitalocean.app',
+        pathname: '/books/**',
+      },
     ],
   },
 };
